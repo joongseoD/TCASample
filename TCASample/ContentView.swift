c34886fe508ba4cb9b91ced5893424c0e47189c4
Damor <damor.js@kakaostyle.com>
2023-03-26 18:56:14 +0900

    Initial Commit

---
+++ TCASample/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TCASample
+//
+//  Created by 데이머_동중서 on 2023/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TCASample/TCASampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCASampleApp.swift
+//  TCASample
+//
+//  Created by 데이머_동중서 on 2023/03/26.
+//
+
+import SwiftUI
+
+@main
+struct TCASampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
